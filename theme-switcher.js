dc093700da1959503ef33bb7a525436353a5194b
Ste Coleman <stecoleman1@gmail.com>
2020-06-16 10:24:19 +0100

    Mainly styling finishing touches. clean up of js code and class changes

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,11 +1,17 @@
-
+/* Setting a variable for the stylesheet link that contains the
+dartkmode theme as this is the initial colorscheme */
 var themeStyle = document.getElementById("themeSwitcher");
 
+/* this function checks to see what colourscheme is in place and decides
+what to change the "href" attribute's inner html to */
 function themeColor() {
-  if (themeStyle.getAttribute("href") === "css/darkmode.css"){
+  if (themeStyle.getAttribute("href") === "css/darkmode.css") {
     themeStyle.setAttribute("href", "css/lightmode.css");
-  }else { themeStyle.setAttribute("href", "css/darkmode.css");
+  } else {
+    themeStyle.setAttribute("href", "css/darkmode.css");
   }
 }
 
- document.getElementById("switcher").addEventListener("click", themeColor);
+/* adding a event listener to the slider switch at the top of the page. once clicked
+on this calls the function themeColor that */
+document.getElementById("switcher").addEventListener("click", themeColor);
